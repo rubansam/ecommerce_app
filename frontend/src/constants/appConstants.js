2500532490925d12b4dffbe03443e867c26cc06f
Ruban-3107 <ruba.antonymech@gamil.com>
2025-06-19 16:01:51 +0530

    eccommerce_web_setup

--- frontend/src/constants/appConstants.js
+++
@@ -1,7 +0,0 @@
-const APPCONSTANTS = {
-  AUTHTOKEN: 'authToken',
-  USER_ID: 'userId',
-  // Add other app-wide constants here
-};
-
-export default APPCONSTANTS; 
--- frontend/src/redux/actionTypes/productActionTypes.js
+++
@@ -1,5 +0,0 @@
-export const PRODUCT_ACTION_TYPES = {
-  FETCH_PRODUCTS_REQUEST: 'products/fetchProductsRequest',
-  FETCH_PRODUCTS_SUCCESS: 'products/fetchProductsSuccess',
-  FETCH_PRODUCTS_FAILURE: 'products/fetchProductsFailure',
-}; 
